24db3bdf716badfc3bd3719565109d02e0c7eef0
Chandan Singh <chandan_singh@berkeley.edu>
2020-06-11 21:54:29 +0000

    call transects function

--- src/transects/make_transects.py
+++ src/transects/make_transects.py
@@ -8,15 +8,16 @@ from argparse import ArgumentParser
 import pandas as pd
 from os.path import join as oj
 
-def make_transects(config,
-                   G,
-                   attr,
-                   N_IMS_LIST: list,
-                   LIMS_LIST: list,
+def make_transects(G,
+                   attr: list=['A', 'C', 'H', 'G'],
+                   N_IMS_LIST: list=[1, 2, 2, 2],
+                   LIMS_LIST: list=[0.5, 0.5, -1.5, 1.7, -0.5, 0.0, -1.75, 1.75],
                    N: int=10,
+                   orth: bool=False,
                    save_dir: str='results',
                    randomize_seeds: bool=False,
                    model_dir: str="./data/latent-models/",
+                   latents: np.ndarray=None,
                    seed_path: str="./data/annotation-data/W.npy"):
     '''
     Params
@@ -25,6 +26,15 @@ def make_transects(config,
         Number of transects to generate
     N_IMS_LIST
         List of number of images to generate in each direction
+        
+    latents
+        Numpy of array to start making latents
+    randomize_seeds
+        If this is true and latents not passed, generate random latents
+    seed_path
+        If latents is None and randomize_seeds is False, then
+        generate using seeds at this path
+        
     '''
     
     # Output dir
@@ -36,15 +46,19 @@ def make_transects(config,
     attrs_all = {
         a: [] for a in attr
     }
+    attrs_all['fnames'] = []
     
     # Set up gpu
 #     os.environ["CUDA_VISIBLE_DEVICES"] = str(config.gpu)
     
-    # Latent codes to use
-    if not config.randomize_seeds:
-        W_all = np.load(seed_path)
-    else:
+    # Latent codes to use (should end up (N, 512) or (N, 18, 512))
+    if latents is not None:
+        W_all = latents
+    elif randomize_seeds:
         W_all = G.getStyle(np.random.randn(N, 512))    
+    else:
+        W_all = np.load(seed_path)
+    N = min(N, W_all.shape[0])
 
     # Attributes coded by letters
     all_attrs = 'HAGCBMSEW'
@@ -74,7 +88,7 @@ def make_transects(config,
         v = normalize(plane.coef_)
         norm = np.linalg.norm(plane.coef_)
 
-        if config.orth:
+        if orth:
             v_orth = orthogonalize(v, planes[0:idx] + planes[idx+1:])
             scale = np.sum(v * v_orth) # Need to change point spacing to account for altered direction.
             dirs.append(v_orth)
@@ -94,6 +108,7 @@ def make_transects(config,
     # Offset vectors from boundary in latent space
     deltas = sum( [ np.expand_dims(dirs[i],0) * np.expand_dims(grids[i],-1) 
                     for i in range(len(grids)) ] )
+    z_dim = G.Gs.input_shape[1]
     deltas = np.reshape(deltas, (-1, z_dim), 'F')
 
     batch_size = 1 
@@ -125,7 +140,7 @@ def make_transects(config,
             fname = f'{save_dir}/{ex_num:04d}'
             for a,b in zip(attr, idx):
                 fname += '_' + a + str(int(b))
-
+            attrs_all['fnames'].append(fname)
             imageio.imwrite(fname + '.jpg', img[0, ...])           
             
             
@@ -219,15 +234,16 @@ if __name__ == "__main__":
     parser.add_argument('--gpu', type=int)    
     parser.add_argument('--version', type=int, default=1) # not used
     config = parser.parse_args()
+    print('config', config, '\n\n')
     
     # Get GAN
     np.random.seed(2)
     G = ganwrapper.GANWrapper(image_size=512)
-    z_dim = G.Gs.input_shape[1]
-    make_transects(config, G,
+    make_transects(G,
          N_IMS_LIST=config.L,
          attr=config.attr,
          N=config.N,
+         orth=config.orth,
          LIMS_LIST=config.lims,
          save_dir=config.save_dir,
          randomize_seeds=config.randomize_seeds
