f8cdf97976be7fbb26f1bc4465b0e975c0cc7b44
Chandan Singh <chandan_singh@berkeley.edu>
2020-06-11 00:20:37 +0000

    make celeba square

--- models/stylegan2encoder/project.sh
+++ models/stylegan2encoder/project.sh
@@ -1,5 +1,5 @@
 # celeba generated
-# python align_images.py ../../data/celeba-hq/images ../../data_processed/celeba-hq/aligned_images
+# python align_images.py ../../data/celeba-hq-guha/images ../../data_processed/celeba-hq/aligned_images
 # python project_images.py ../../data/celeba-hq/images/ ../../data_processed/celeba-hq/generated_images/
 
 # stylegan2 generated
@@ -9,4 +9,4 @@
 
 # python project_images.py ../../data/annotation-dataset-stylegan2/images/ ../../data_processed/stylegan2/generated_images_1/ --regularize_mean_deviation_weight 1
 
-python project_images.py ../../data/annotation-dataset-stylegan2/images/ ../../data_processed/stylegan2/generated_images_0.1/ --regularize_mean_deviation_weight 0.1
+python project_images.py ../../data/CelebA/Img/img_square_celeba/ ../../data_processed/celeba_real_square/generated_images_0/ --regularize_mean_deviation_weight 0
