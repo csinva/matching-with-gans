d6ff8df2550bd02f9d6f381097b1f97e54efca82
Chandan Singh <chandan_singh@berkeley.edu>
2020-06-13 19:51:31 +0000

    project cropped faces

--- models/stylegan2encoder/project.sh
+++ models/stylegan2encoder/project.sh
@@ -2,24 +2,48 @@
 # python align_images.py ../../data/celeba-hq-guha/images ../../data_processed/celeba-hq/aligned_images
 # python project_images.py ../../data/celeba-hq/images/ ../../data_processed/celeba-hq/generated_images/
 
-# stylegan2 generated
-# python project_images.py ../../data/annotation-dataset-stylegan2/images/ ../../data_processed/stylegan2/generated_images_0/ --regularize_mean_deviation_weight 0
 
-# python project_images.py ../../data/annotation-dataset-stylegan2/images/ ../../data_processed/stylegan2/generated_images_10000/ --regularize_mean_deviation_weight 10000
+
+
+
+
+# stylegan2 generated #############################
+# python project_images.py ../../data/annotation-dataset-stylegan2/images/ ../../data_processed/stylegan2/generated_images_0/ --regularize_mean_deviation_weight 0
 
 # python project_images.py ../../data/annotation-dataset-stylegan2/images/ ../../data_processed/stylegan2/generated_images_1/ --regularize_mean_deviation_weight 1
 
-# python project_images.py ../../data/CelebA/Img/img_square_celeba/ ../../data_processed/celeba_real_square/generated_images_0/ --regularize_mean_deviation_weight 0
+# python project_images.py ../../data/annotation-dataset-stylegan2/images/ ../../data_processed/stylegan2/generated_images_10000/ --regularize_mean_deviation_weight 10000
+
+
+
+
+
+
+# celeba-hq generated #############################
 
 # python project_images.py ../../data/celeba-hq/ims/ ../../data_processed/celeba-hq/generated_images_0/ --regularize_mean_deviation_weight 0
+
+# python project_images.py ../../data/celeba-hq/ims/ ../../data_processed/celeba-hq/generated_images_0.01/ --regularize_mean_deviation_weight 0.01
+
+# python project_images.py ../../data/celeba-hq/ims/ ../../data_processed/celeba-hq/generated_images_0.1/ --regularize_mean_deviation_weight 0.1
+
+# python project_images.py ../../data/celeba-hq/ims/ ../../data_processed/celeba-hq/generated_images_1/ --regularize_mean_deviation_weight 1
 
 # python project_images.py ../../data/celeba-hq/ims/ ../../data_processed/celeba-hq/generated_images_10/ --regularize_mean_deviation_weight 10 # not really worth it
 
 
 # python project_images.py ../../data/celeba-hq/ims/ ../../data_processed/celeba-hq/generated_images_10000/ --regularize_mean_deviation_weight 10000
 
-# python project_images.py ../../data/celeba-hq/ims/ ../../data_processed/celeba-hq/generated_images_0.01/ --regularize_mean_deviation_weight 0.01
 
-# python project_images.py ../../data/celeba-hq/ims/ ../../data_processed/celeba-hq/generated_images_1/ --regularize_mean_deviation_weight 1
 
-python project_images.py ../../data/celeba-hq/ims/ ../../data_processed/celeba-hq/generated_images_0.1/ --regularize_mean_deviation_weight 0.1
+
+
+
+
+# project real ims #############################
+
+python project_images.py ../../data_processed/pilot-our-images/G2P2/ ../../data_processed/pilot-our-images/G2P2_proj/ --regularize_mean_deviation_weight 0.1
+
+python project_images.py ../../data_processed/pilot-our-images/KFS/ ../../data_processed/pilot-our-images/KFS_proj/ --regularize_mean_deviation_weight 0.1
+
+python project_images.py ../../data_processed/pilot-our-images/PPP/ ../../data_processed/pilot-our-images/PPP_proj/ --regularize_mean_deviation_weight 0.1
