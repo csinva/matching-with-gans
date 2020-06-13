b66465f5e74cdcc34304dd0aeaef887be3f8352d
Chandan Singh <chandan_singh@berkeley.edu>
2020-06-13 00:25:28 +0000

    remapping latents

--- src/process_latents.py
+++ src/process_latents.py
@@ -35,6 +35,31 @@ attr_map = {
     }
 ks = sorted(attr_map.keys())
 
+def load_labs(celeba_labs_fname='../data/celeba-hq/Anno/list_attr_celeba.txt'):
+    labs_full = pd.read_csv(celeba_labs_fname, delim_whitespace=True, skiprows=1)
+    labs = pd.DataFrame()
+    # print(labs.keys())
+    # print(labs_full.keys())
+
+    # large is more male
+    labs['gender'] = labs_full['Male']
+
+    # larger is longer
+    labs['hair-length'] = -1 * labs_full['Bald'] # Bangs, Receding_Hairline
+
+    # larger is more
+    labs['facial-hair'] = labs_full['Mustache'] # Goatee, Mustache, No_Beard, 5_o_Clock_Shadow
+
+    # higher is more
+    labs['makeup'] = labs_full['Heavy_Makeup'] # Wearing_Lipstick
+
+    # higher is darker
+    labs['skin-color'] = labs_full['Pale_Skin'] * -1
+
+    # older is more positive
+    labs['age'] = labs_full['Young'] * -1
+    return labs
+
 if __name__ == '__main__':
     network_pkl = 'gdrive:networks/stylegan2-ffhq-config-f.pkl'
     _, _, Gs = pretrained_networks.load_networks(network_pkl)
@@ -48,32 +73,6 @@ if __name__ == '__main__':
     coefs, intercepts = make_transects.get_directions(all_attrs=all_attrs)
     coefs = np.array(coefs).squeeze()
     intercepts = np.array(intercepts).flatten()
-
-
-    def load_labs(celeba_labs_fname='../data/celeba-hq/Anno/list_attr_celeba.txt'):
-        labs_full = pd.read_csv(celeba_labs_fname, delim_whitespace=True, skiprows=1)
-        labs = pd.DataFrame()
-        # print(labs.keys())
-        # print(labs_full.keys())
-
-        # large is more male
-        labs['gender'] = labs_full['Male']
-
-        # larger is longer
-        labs['hair-length'] = -1 * labs_full['Bald'] # Bangs, Receding_Hairline
-
-        # larger is more
-        labs['facial-hair'] = labs_full['Mustache'] # Goatee, Mustache, No_Beard, 5_o_Clock_Shadow
-
-        # higher is more
-        labs['makeup'] = labs_full['Heavy_Makeup'] # Wearing_Lipstick
-
-        # higher is darker
-        labs['skin-color'] = labs_full['Pale_Skin'] * -1
-
-        # older is more positive
-        labs['age'] = labs_full['Young'] * -1
-        return labs
 
     labs = load_labs()
 
